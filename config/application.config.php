bade0d693a2239a4945ebaf45a320596c66829ca
Refael Gold <refaelgold@gmail.com>
2014-02-23 13:13:41 +0200

    add wordpress link on header + wordpress blog (blog.nirgoldman.com)

--- config/application.config.php
+++ config/application.config.php
@@ -11,7 +11,7 @@ return array(
         'StartTech',
         'Album',
         'SpotOption',
-        'ZendDeveloperTools',
+//        'ZendDeveloperTools',
         'DoctrineModule',
         'DoctrineORMModule',
         'Kcs',
